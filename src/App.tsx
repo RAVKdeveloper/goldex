f986c7609db566c993930e0b84323c96efb868ea
unknown <shcherbakovkirill2@gmail.com>
2024-03-20 12:12:07 +0300

    test husky two

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   const a = 'werewrwer'
 
   return (
-    <div className='text-3xl text-red-800'>
-      hello43tk4-t6yo045555555555555555555555555555555555555555555555oi75
+                                           <div className='text-3xl text-red-800'>
+                                                       hello43tk4-t6yo045555555555555555555555555555555555555555555555oi75
       4i7i54i9749i079506i7i906790i560i9860i98i096806
       {a}
     </div>
