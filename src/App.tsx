8f8ae85d7f730686d1dacd6feec55d7c2912c5ff
unknown <shcherbakovkirill2@gmail.com>
2024-03-20 18:20:18 +0300

    i18next version two

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,7 @@ import { useTranslation } from 'react-i18next'
 const App: FC = () => {
   const { t } = useTranslation()
 
-  return (
-    <div>
-      {t('hello')}
-      {t('check')}
-    </div>
-  )
+  return <div>{t('demo')}</div>
 }
 
 export default App
