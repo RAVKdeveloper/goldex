137c970d7428e84f5ce90801c84214908a40a38f
Yana Danilishina <niusia1nessa@gmail.com>
2024-04-03 16:48:29 +0300

    fix: add translation

--- src/entities/Forms/ResetPasswordForm/ui/ResetPasswordForm.entity.tsx
+++ src/entities/Forms/ResetPasswordForm/ui/ResetPasswordForm.entity.tsx
@@ -1,23 +1,27 @@
 import { FC } from 'react'
 import { Button, TextField } from 'goldex-ui-library'
 
+import { useTranslation } from 'react-i18next'
+
 import s from '../assets/style.module.css'
 
 export const ResetPasswordFormEntity: FC = () => {
+  const { t } = useTranslation()
+
   return (
     <form className={s.form}>
       <div className={s.header}>
-        <h2 className={s.title}>Безопасность</h2>
-        <p className={s.supportingText}>Обновите свой пароль</p>
+        <h2 className={s.title}>{t('resetPasswordTitle')}</h2>
+        <p className={s.supportingText}>{t('resetPasswordSupText')}</p>
       </div>
       <div className={s.content}>
-        <TextField label='Текущий пароль' sizeType='lg' />
-        <TextField label='Новый пароль' sizeType='lg' />
-        <TextField label='Подтвердите новый пароль' sizeType='lg' />
+        <TextField label={t('labels.currentPassword')} sizeType='lg' />
+        <TextField label={t('labels.newPassword')} sizeType='lg' />
+        <TextField label={t('labels.confirmCurrentPassword')} sizeType='lg' />
       </div>
       <div className={s.button}>
         <Button variant='primary' size='es' disabled>
-          Сохранить
+          {t('btn.save')}
         </Button>
       </div>
     </form>
