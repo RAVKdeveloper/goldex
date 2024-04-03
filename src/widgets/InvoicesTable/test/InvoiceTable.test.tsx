073c618302422d13163ef19acd2419b32a948ce5
unknown <shcherbakovkirill2@gmail.com>
2024-04-03 17:47:44 +0300

    fix: test invoices problems

--- src/widgets/InvoicesTable/test/InvoiceTable.test.tsx
+++ src/widgets/InvoicesTable/test/InvoiceTable.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/shared/service/api/api.instance'
 import { InvoicesTableWidget } from '../..'
 
 import * as reduxHooks from '../../../shared/service/store/hooks/hooks'
@@ -22,7 +24,11 @@ describe('testing InvoicesTableWidget', () => {
     jest.spyOn(reduxHooks, 'useAppSelector').mockReturnValue({})
     jest.spyOn(reduxHooks, 'useAppDispatch')
 
-    const component = render(<InvoicesTableWidget />)
+    const component = render(
+      <QueryClientProvider client={queryClient}>
+        <InvoicesTableWidget />
+      </QueryClientProvider>,
+    )
 
     const tableComponent = screen.getByTestId('table')
 
