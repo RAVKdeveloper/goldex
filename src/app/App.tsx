f33bde80c5f6d0f7eb037a593a6a5268fe7535f4
unknown <shcherbakovkirill2@gmail.com>
2024-04-04 15:23:04 +0300

    fead: add error app handling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable react-refresh/only-export-components */
 import { FC } from 'react'
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Provider } from 'react-redux'
+import { withErrorBoundary } from 'react-error-boundary'
 
 import { queryClient } from '@/shared/service/api/api.instance'
 import { store } from '@/shared/service/store/store'
@@ -22,4 +24,6 @@ const App: FC = () => {
   )
 }
 
-export default App
+export default withErrorBoundary(App, {
+  fallback: <div>Произошла ошибка</div>,
+})
